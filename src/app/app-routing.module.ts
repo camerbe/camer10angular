ac5ea7439258380b792034386f25b35d94b05aab
tristan <tristanvellozzi@gmail.com>
2023-11-27 15:52:50 +0100

    Le login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Admin/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    title: 'login',
+    component:LoginComponent
+  },
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
